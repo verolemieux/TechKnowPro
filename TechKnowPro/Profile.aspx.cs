1bde7fab4b1606a07a19b1a634e30e3d6e22ad79
coolbians <ianmiranda910@gmail.com>
2019-03-07 18:07:29 -0500

    Create Profile.aspx.cs
    also Profile.aspx.cs

---
+++ TechKnowPro/Profile.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TechKnowPro
+{
+    public partial class Profile : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["UserType"] == null || Session["UserType"].ToString() != "tech")
+            {
+                Session["ErrorMessage"] = "You do not have permission to access this page.";
+                if (Session["UserName"] == null) Response.Redirect("Login.aspx");
+                Response.Redirect("Home.aspx");
+            }
+
+
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database.mdf;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = "SELECT * FROM [User] WHERE Username = '" + Session["Username"].ToString();
+            cmd.ExecuteNonQuery();
+            SqlDataReader sdr = cmd.ExecuteReader();
+
+            if (sdr.Read())
+            {
+                txtFirstName.Text = "First Name";
+            }
+        }
+    }
+}
