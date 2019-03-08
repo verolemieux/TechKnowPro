a91b780e7df4e6551f700eb7c0fb52a3369717f8
coolbians <ianmiranda910@gmail.com>
2019-03-08 14:39:03 -0500

    fixing if-statement in Profile.aspx.cs  auto-filling contact info from DB
    still needs to retrieve Password from db and output astericks
    
    needs new column "Profile name" in db so that it *may* be updated by customer

--- TechKnowPro/Profile.aspx.cs
+++ TechKnowPro/Profile.aspx.cs
@@ -12,26 +12,39 @@ namespace TechKnowPro
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (Session["UserType"] == null || Session["UserType"].ToString() != "tech")
+            
+            if (Session["UserType"] == null || Session["UserType"].ToString() != "customer")
             {
                 Session["ErrorMessage"] = "You do not have permission to access this page.";
                 if (Session["UserName"] == null) Response.Redirect("Login.aspx");
                 Response.Redirect("Home.aspx");
             }
 
-
+            
             SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database.mdf;Integrated Security=True");
             con.Open();
             SqlCommand cmd = con.CreateCommand();
             cmd.CommandType = System.Data.CommandType.Text;
-            cmd.CommandText = "SELECT * FROM [User] WHERE Username = '" + Session["Username"].ToString();
+            cmd.CommandText = "SELECT * FROM [User] WHERE Username = '" + Session["Username"].ToString()+ "'";
             cmd.ExecuteNonQuery();
             SqlDataReader sdr = cmd.ExecuteReader();
 
             if (sdr.Read())
             {
-                txtFirstName.Text = "First Name";
+                // doesn't exist in the table
+                // txtProfileName.Text = sdr["Profile_Name"].ToString();
+
+                // show password in astericks
+                // txtPassword.Text = sdr["Password"].ToString();
+                
+
+                txtUsername.Text = sdr["Username"].ToString();
+                txtFirstName.Text = sdr["First_Name"].ToString();
+                txtLastName.Text = sdr["Last_Name"].ToString();
+                txtAddress.Text = sdr["Address"].ToString();
+                txtEmail.Text = sdr["Username"].ToString();
             }
+            
         }
     }
 }
