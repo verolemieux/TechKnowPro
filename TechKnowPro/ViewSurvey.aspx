013ecfea6dcfb53900c3bfff91a07251012bec64
sparksflyupwards <saad.jamil.khan@gmail.com>
2019-03-16 18:16:53 -0400

    adding admin pages

---
+++ TechKnowPro/ViewSurvey.aspx
@@ -0,0 +1,113 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ViewSurvey.aspx.cs" Inherits="TechKnowPro.ViewSurvey" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style3 {
+            margin-top: 0px;
+        }
+        </style>
+    <link href="css/bootstrap.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table class="auto-style1">
+                <tr>
+                    <td><h1><b>TechKnow Pro - Incident Management Software</b></h1></td>
+                    <td>
+                        <asp:Button ID="btnLogout" class="btn btn-outline-primary" runat="server" Text="Logout" OnClick="btnLogout_Click" CausesValidation="False" />
+                    </td>
+                </tr>
+            </table>
+        </div>
+
+         <table class="auto-style1">
+                    
+                        Select a customer
+                        <p>
+                            <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" DataSourceID="SqlDataSource1" DataTextField="Username" DataValueField="Username" AppendDataBoundItems = "true" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+                                <asp:ListItem Value="choose a customer">-- choose a customer --</asp:ListItem>
+                    
+                            </asp:DropDownList>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="Please select a user." ControlToValidate="DropDownList1"></asp:RequiredFieldValidator>
+                            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT * FROM [User] WHERE NOT Username='admin@isp.net' AND NOT Username = 'tech@isp.net'">
+                        
+                            </asp:SqlDataSource>
+                        &nbsp;
+                         </p>
+                    
+                </table>
+        <asp:Panel runat="server" ID="selectCustomerPanel" Visible="false">
+               
+                <p>
+                    <asp:Label runat="server" ID="surveyListingLabel">Survey Listing</asp:Label>
+                    <br/>
+                    <asp:SqlDataSource ID="SqlDataSource2" runat="server"  ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT [incident_id] FROM [Surveys] "></asp:SqlDataSource>
+                    <asp:ListBox ID="ListBox1" runat="server" Width="546px" OnSelectedIndexChanged="ListBox1_SelectedIndexChanged" >
+                        <asp:ListItem Enabled="False">Select a submitted survey</asp:ListItem>
+                    </asp:ListBox>
+
+                </p>
+                <p>
+                    <asp:Button ID="btnRetrieveSurveyInformation" class="btn btn-outline-primary" runat="server" Text="Retrieve Survey Information" CausesValidation="False" OnClick="retrieveSurveyInformation" /> 
+
+                </p>
+        </asp:Panel>
+        <asp:Panel runat="server" ID="surveyInfo" Visible="false">
+                    <asp:Table ID="customerRatingTable" runat="server" Height="158px" Width="899px" CssClass="auto-style3">
+                        <asp:TableHeaderRow>
+                            <asp:TableHeaderCell>Customer Rating</asp:TableHeaderCell> 
+                        </asp:TableHeaderRow>
+                        <asp:TableRow>
+                            <asp:TableCell> Response Time:
+                                            <asp:Label ID="lblResponseTime" runat="server"></asp:Label></asp:TableCell>
+                            <asp:TableCell> Technician Efficiency:
+                                            <asp:Label ID="lblTechEfficiency" runat="server"></asp:Label></asp:TableCell>
+                            <asp:TableCell> Problem Resolution:
+                                            <asp:Label ID="lblProblemResolution" runat="server"></asp:Label></asp:TableCell>
+                        </asp:TableRow>
+                   
+
+                        <asp:TableRow>
+                            <asp:TableCell>
+                                 Contact to discuss incident:
+                                 <asp:Label ID="lblRequestContact" runat="server"></asp:Label>
+                            </asp:TableCell>
+
+                            <asp:TableCell>
+                                  Preferred contact method:
+                                  <asp:Label ID="lblContactMethod" runat="server"></asp:Label>
+                            </asp:TableCell>
+                        </asp:TableRow>
+
+                
+
+
+                    </asp:Table>
+
+                 <p>
+                     &nbsp;</p>
+                  <br />
+                    <br />
+                Additional Information:<br />
+                            <asp:Panel ID="Panel2" runat="server" CssClass="auto-style3" Height="230px" Width="450px" BorderStyle="Solid">
+                                <asp:Label ID="lblAdditionalInfo" runat="server"></asp:Label>
+                            </asp:Panel>
+               
+                    <br />
+       </asp:Panel>
+
+            <center> <asp:Button ID="btnHome" class="btn btn-outline-primary" runat="server" Text="Home" CausesValidation="False" OnClick="btnHome_Click" /></center>
+        <br />
+        <br />
+     <hr />
+            <h6 class="font-weight-light">@2019 COMP2139 TechKnow Pro </h6> </form>
+</body>
+</html>
---
+++ TechKnowPro/ViewSurvey.aspx.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace TechKnowPro
+{
+    public partial class ViewSurvey : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+            if (Session["User Type"] == null) {
+                Session["ErrorMessage"] = "You do not have permission to access this page.";
+                Response.Redirect("Login.aspx");
+                 
+            }
+            else if (Session["User Type"].ToString() != "admin")
+            {
+                Session["ErrorMessage"] = "You do not have permission to access this page.";
+                Response.Redirect("Home.aspx");
+            }
+
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database.mdf;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = "SELECT * FROM [User] where not Username='tech@isp.net' AND not Username='admin@isp.net'";
+            cmd.ExecuteNonQuery();
+            SqlDataReader sdr = cmd.ExecuteReader();
+          
+            con.Close();
+        }
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            
+
+            if(DropDownList1.SelectedValue=="choose a customer")
+            {
+                surveyInfo.Visible = false;
+                selectCustomerPanel.Visible = false;
+                return;
+            }
+
+            surveyInfo.Visible = false;
+            selectCustomerPanel.Visible = true;
+
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database.mdf;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = "SELECT * FROM [User] WHERE Username='" + DropDownList1.SelectedValue.ToString() + "'";
+            cmd.ExecuteNonQuery();
+            SqlDataReader sdr = cmd.ExecuteReader();
+          
+            con.Close();
+
+            ListBox1.Items.Clear();
+            con.Open();
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = "Select incident_id FROM [Surveys] WHERE Username='" + DropDownList1.SelectedValue + "'";
+            cmd.ExecuteNonQuery();
+             sdr = cmd.ExecuteReader();
+            if (sdr.HasRows)
+            {
+                while (sdr.Read())
+                {
+                    ListItem surveyItem = new ListItem();
+                    surveyItem.Text = "Select the survey for incident number: " + sdr["incident_id"].ToString();
+                    surveyItem.Value = sdr["incident_id"].ToString();
+                    ListBox1.Items.Add(surveyItem);
+                }
+            }
+            else
+            {
+                ListItem surveyItem = new ListItem();
+                surveyItem.Text = "This user does not have a survey on file";
+                surveyItem.Value = "";
+                ListBox1.Items.Add(surveyItem);
+            }
+            con.Close();
+        }
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            Session.Abandon();
+            Response.Redirect("Login.aspx");
+        }
+
+        protected void btnHome_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Home.aspx");
+        }
+
+        protected void SqlDataSource1_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+        {
+
+        }
+
+       
+
+        protected void retrieveSurveyInformation(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+
+            if (ListBox1.SelectedValue == "")
+            {
+                return;
+            }
+
+            surveyInfo.Visible = true;
+            selectCustomerPanel.Visible = true;
+
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database.mdf;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = "SELECT * FROM [Surveys] WHERE Username='" + DropDownList1.SelectedValue.ToString() + "' AND incident_id = '" + ListBox1.SelectedValue.ToString() + "'";
+            cmd.ExecuteNonQuery();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            
+
+            if (sdr.HasRows)
+            {
+                while (sdr.Read())
+                {
+                    lblAdditionalInfo.Text = (sdr["additional_comments"].ToString() == "" ? " N/A" : sdr["additional_comments"].ToString());
+                    lblContactMethod.Text = (sdr["contact_preference"].ToString() == "" ? " N/A" : sdr["contact_preference"].ToString());
+                    lblRequestContact.Text = (sdr["contact_further"].ToString() == "" ? " N/A" : sdr["contact_further"].ToString());
+                    lblProblemResolution.Text = (sdr["problem_resolution"].ToString() == "" ? " N/A" : sdr["problem_resolution"].ToString());
+                    lblTechEfficiency.Text = (sdr["technician_efficiency"].ToString() == "" ? " N/A" : sdr["technician_efficiency"].ToString());
+                    lblResponseTime.Text = (sdr["response_time"].ToString() == "" ? " N/A" : sdr["response_time"].ToString());
+
+                }
+            }
+
+            con.Close();
+
+        }
+    }
+}
